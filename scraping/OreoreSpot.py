2b4d831d02575dfe657cf0fcfbc97ec79a1b3755
kzysd <nuis.ysd@gmail.com>
2019-03-13 14:37:23 +0900

    [add]INSERT用モジュールを追加

---
+++ scraping/OreoreSpot.py
@@ -0,0 +1,10 @@
+class OreoreSpot(object):
+    def __init__(self, name, description, oreore_genre_id, \
+            lon, lat, image, access_text):
+        self.name = name
+        self.description = description
+        self.oreore_genre_id = oreore_genre_id
+        self.lon = lon
+        self.lat = lat
+        self.image = image
+        self.access_text = access_text
---
+++ scraping/Storer.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import configparser
+import mysql.connector
+
+from OreoreSpot import OreoreSpot
+
+_config = configparser.ConfigParser()
+_config.read('development.cfg')
+_ACCOUNT = _config['MySQL']
+
+class Storer(object):
+    '''stores spot metadata into MySQL database.
+
+    usage:
+        with Storer() as storer:
+            spots = ...
+            storer.store(spots)
+    or
+        storer = Storer()
+        storer.connect()
+        storer.store(spots)
+        storer.close()
+    '''
+
+    JARAN = 'jaran'
+    GURUTABI = 'gurutabi'
+
+    def __init__(self, account=_ACCOUNT):
+        self.account = account
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def connect(self):
+        self.conn = mysql.connector.connect(
+          db      = self.account['db'],
+          host    = self.account['host'],
+          user    = self.account['user'],
+          passwd  = self.account['passwd'],
+          charset = 'utf8')
+
+    def close(self):
+        self.conn.close()
+
+    def insert_spot(self, spot):
+        cur = self.conn.cursor()
+        q = 'INSERT INTO spot \
+             (name, description, genre_id, lon, lat, image, access_text) \
+             VALUES (%s, %s, %s, %s, %s, %s, %s)'
+        cur.execute(q, ( \
+            spot.name, \
+            spot.description, \
+            spot.oreore_genre_id, \
+            spot.lon, \
+            spot.lat, \
+            spot.image, \
+            spot.access_text \
+        ))
+        self.conn.commit()
+        cur.close()
+
+    def store(self, spots):
+        map(self.insert_spot, spots)
+
+    def map_oreoere_and_sites(self, genre_small, site_name):
+        cur = self.conn.cursor()
+        # [TODO] テーブル名に引数そのまま入れるのはヤバい
+        q = 'SELECT {0}_genre.oreore_genre_id \
+             FROM {0}_genre NATURAL JOIN {0}_genre_small \
+             WHERE genre_small=%s'.format(site_name)
+        cur.execute(q, (genre_small, ))
+        ore_itr = cur.fetchall()
+        cur.close()
+        return [x[0] for x in ore_itr]
